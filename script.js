80355ee6234fb5bb232387e76b5a2a475f3d5694
Enes Utku Cakmak <cakmakenesutku@gmail.com>
2026-02-11 22:08:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function startMusic(){
+    document.body.classList.add("active");
+    document.getElementById("bgMusic").play();
+}
+
+function toggleSocial(element){
+    element.classList.toggle("active");
+}
